a7dc9d6e1a9bf28c846c6aaa1580f5d1d5865270
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2024-11-16 18:15:06 -0300

    Remove unused fetchData function from data.ts

--- src/lib/data.ts
+++
@@ -1,5 +0,0 @@
-export const fetchData = async () => {
-    const response = await fetch('https://eng-soft-proj-back.vercel.app/');
-    const data = await response.json();
-    console.log(data);
-  };
