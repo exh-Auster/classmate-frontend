ae8adfcbd3ec1ed84b5fa8d776ac3bc6ae3a8a79
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2024-11-18 20:27:48 -0300

    Add LogOut button to UserCard component and adjust layout

--- src/components/userCard.tsx
+++ src/components/userCard.tsx
@@ -3,6 +3,8 @@ import { Avatar, /*AvatarImage,*/ AvatarFallback } from "@/components/ui/avatar"
 import { Card, CardHeader } from "./ui/card"
 import { UserProps } from "@/app/page"
 import { MouseEventHandler } from "react"
+import { Button } from "./ui/button"
+import { LogOut } from "lucide-react"
 // import { currentUser } from "@/app/page"
 
 interface UserCardProps {
@@ -13,19 +15,24 @@ interface UserCardProps {
 function UserCard({ user, onClick }: UserCardProps) {
     return (
         <Card className="cursor-pointer" onClick={onClick}>
-            <CardHeader className="flex flex-row items-center gap-4">
-                <Avatar>
-                    {/* <AvatarImage src={user.avatar} alt={user.name} /> */}
-                    <AvatarFallback>{user.name[0]}</AvatarFallback>
-                </Avatar>
-                <div>
-                    <h2 className="text-lg font-semibold">{user.name}</h2>
-                    <div className="text-sm text-gray-500">
-                        <span>{user.following} Seguindo</span>
-                        <span className="mx-1">•</span>
-                        <span>{user.followers} Seguidores</span>
+            <CardHeader className="flex flex-row items-center justify-between">
+                <div className="flex items-center gap-4">
+                    <Avatar>
+                        {/* <AvatarImage src={user.avatar} alt={user.name} /> */}
+                        <AvatarFallback>{user.name[0]}</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <h2 className="text-lg font-semibold">{user.name}</h2>
+                        <div className="text-sm text-gray-500">
+                            <span>{user.following} Seguindo</span>
+                            <span className="mx-1">•</span>
+                            <span>{user.followers} Seguidores</span>
+                        </div>
                     </div>
                 </div>
+                <Button variant="ghost" /*onClick={}*/ disabled>
+                    <LogOut className="mr-0 h-4 w-4" />
+                </Button>
             </CardHeader>
         </Card>
     )
