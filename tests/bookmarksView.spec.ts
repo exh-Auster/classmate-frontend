8fae88bd0ff8e6cd87a1793b0bee36e0f72b2d02
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2024-11-23 20:07:02 -0300

    Improve test structure

--- tests/bookmarksView.spec.ts
+++ tests/bookmarksView.spec.ts
@@ -2,10 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('https://classmate-front.vercel.app/');
-  });
+});
 
-test('should display bookmarks header', async ({ page }) => {
-  await page.click('[data-testid="bookmarks-link"]');
+test.describe('bookmarksView', () => {
+    test('should display bookmarks header', async ({ page }) => {
+        await page.click('[data-testid="bookmarks-link"]');
 
-  await expect(page.getByTestId('bookmarks-header')).toBeVisible();
+        await expect(page.getByTestId('bookmarks-header')).toBeVisible();
+    })
 });
